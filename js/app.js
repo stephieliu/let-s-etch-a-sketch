63f22731ae7bf37e677f35be996784f8247055a6
stephie <stephieliu1@gmail.com>
2026-01-18 08:12:11 -0500

    added input box and some animations to button/input
    Adjusted grid hover animation duration

--- js/app.js
+++ js/app.js
@@ -41,4 +41,21 @@ for (let i = 0; i<16; i++){
     gridContainer.appendChild(gridRow);
 }
 
+//make reset button to clear the canvas on click
+const resetBtn = document.querySelector('#new-canvas');
 
+//style the button if the mouse is over it
+resetBtn.addEventListener('mouseenter', (e)=>{
+    resetBtn.setAttribute('style', 'box-shadow: 0px 3px 8px #85b0ffff;');
+});
+resetBtn.addEventListener('mouseleave', (e)=>{
+    resetBtn.setAttribute('style', 'box-shadow: none;');
+});
+
+//add styling to the input box and add default text
+const userInput = document.querySelector('#user-num-squares');
+
+userInput.placeholder = 'enter new grid dimensions...';
+// userInput.setAttribute('style', 'color: #cbddff;');
+
+//make the button create a new canvas with user's new dimensions
